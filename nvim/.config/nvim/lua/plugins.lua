777c96cea0a0ffe0390a83cc388cc70d74d7a2d6
Mariano Lambir <mlambir@gmail.com>
2023-01-03 11:37:25 -0300

    tmux nvim integration

--- nvim/.config/nvim/lua/plugins.lua
+++ nvim/.config/nvim/lua/plugins.lua
@@ -98,6 +98,10 @@ return require('packer').startup(function(use)
         end,
     }
     use "lvimuser/lsp-inlayhints.nvim"
+    use({
+        "aserowy/tmux.nvim",
+        config = function() require("tmux").setup() end
+    })
     -- Automatically set up your configuration after cloning packer.nvim
     -- Put this at the end after all plugins
     if packer_bootstrap then
