be8d562b955fed6999fbdaf6cf19f5bf4fa4fb9b
Mariano Lambir <mlambir@gmail.com>
2024-01-20 01:28:48 -0300

    remove mini.comment

--- nvim/lua/custom/plugins/mini.lua
+++ nvim/lua/custom/plugins/mini.lua
@@ -6,15 +6,6 @@ return {
 	version = '*',
 	config = function()
 		require('mini.basics').setup()
-		require('mini.comment').setup()
-		-- require('mini.files').setup({
-		-- 	mappings = {
-		-- 		go_in_plus = '<CR>',
-		-- 	},
-		-- })
-		-- require('mini.jump').setup()
 		require('mini.move').setup()
-
-		-- vim.keymap.set('n', "<leader>e", minifiles_toggle, { desc = "File explorer" })
 	end,
 }
