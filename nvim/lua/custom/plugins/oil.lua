db6c48590cd9936865bd0693f12be9c6b891d20e
Mariano Lambir <mlambir@gmail.com>
2024-01-25 23:38:32 -0300

    oil splits

--- nvim/lua/custom/plugins/oil.lua
+++ nvim/lua/custom/plugins/oil.lua
@@ -5,8 +5,12 @@ return {
   dependencies = { "nvim-tree/nvim-web-devicons" },
   config = function ()
 	local oil = require("oil")
-	oil.setup()
-  	vim.keymap.set('n', "<leader>e", oil.toggle_float, {desc = "File Explorer"})
-  	vim.keymap.set('n', "<leader>E", function() oil.toggle_float(vim.fn.getcwd()) end, {desc = "File Explorer CWD"})
+	oil.setup({
+	  keymaps = {
+	        ["q"] = "actions.close",
+	  }
+	})
+  	vim.keymap.set('n', "<leader>e", oil.open, {desc = "File Explorer"})
+  	vim.keymap.set('n', "<leader>E", function() oil.open(vim.fn.getcwd()) end, {desc = "File Explorer CWD"})
   end
 }
