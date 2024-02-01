f91b6f68d5afe9746c01ea9a78b48117bce1f400
Mariano Lambir <mariano.lambir@shipveho.com>
2024-02-01 10:26:31 -0300

    disable flash search

--- nvim/lua/custom/plugins/flash.lua
+++ nvim/lua/custom/plugins/flash.lua
@@ -2,7 +2,9 @@ return {
   "folke/flash.nvim",
   event = "VeryLazy",
   ---@type Flash.Config
-  opts = {},
+  opts = {
+    search = false
+  },
   -- stylua: ignore
   keys = {
     { "s",     mode = { "n", "x", "o" }, function() require("flash").jump() end,              desc = "Flash" },
