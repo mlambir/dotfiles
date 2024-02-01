d919f769641265316cf4ce1e0c7938d060b1c74f
Mariano Lambir <mariano.lambir@shipveho.com>
2024-02-01 10:26:19 -0300

    copilot stuff

--- nvim/init.lua
+++ nvim/init.lua
@@ -180,6 +180,14 @@ require('lazy').setup({
         component_separators = '|',
         section_separators = '',
       },
+      sections = {
+        lualine_a = { 'mode' },
+        lualine_b = { 'branch' },
+        lualine_c = { 'filename' },
+        lualine_x = { 'encoding', 'fileformat', 'filetype' },
+        lualine_y = { 'progress' },
+        lualine_z = { 'location' },
+      },
     },
   },
 
--- nvim/lua/custom/plugins/ai.lua
+++ nvim/lua/custom/plugins/ai.lua
@@ -13,6 +13,14 @@ local enable_ai = function()
 		return true
 	end
 end
+function fg(name)
+	---@type {foreground?:number}?
+	---@diagnostic disable-next-line: deprecated
+	local hl = vim.api.nvim_get_hl and vim.api.nvim_get_hl(0, { name = name }) or vim.api.nvim_get_hl_by_name(name, true)
+	---@diagnostic disable-next-line: undefined-field
+	local fg = hl and (hl.fg or hl.foreground)
+	return fg and { fg = string.format("#%06x", fg) } or nil
+end
 
 return {
 
@@ -39,41 +47,40 @@ return {
 		config = function()
 			require("copilot_cmp").setup()
 		end
-	}
+	},
 
 	-- copilot status in lualine
 	-- this is taken from the copilot lazyvim extras at:
 	-- https://www.lazyvim.org/plugins/extras/coding.copilot
-	-- {
-	--   "nvim-lualine/lualine.nvim",
-	--   optional = true,
-	--   event = "VeryLazy",
-	--   opts = function(_, opts)
-	--     -- local Util = require("lazyvim.util")
-	--     -- local colors = {
-	--     --   [""] = Util.ui.fg("Special"),
-	--     --   ["Normal"] = Util.ui.fg("Special"),
-	--     --   ["Warning"] = Util.ui.fg("DiagnosticError"),
-	--     --   ["InProgress"] = Util.ui.fg("DiagnosticWarn"),
-	--     -- }
-	--     table.insert(opts.sections.lualine_x, 2, {
-	--       function()
-	--         local icon = require("lazyvim.config").icons.kinds.Copilot
-	--         local status = require("copilot.api").status.data
-	--         return icon .. (status.message or "")
-	--       end,
-	--       cond = function()
-	--         local ok, clients = pcall(vim.lsp.get_active_clients, { name = "copilot", bufnr = 0 })
-	--         return ok and #clients > 0
-	--       end,
-	--       color = function()
-	--         if not package.loaded["copilot"] then
-	--           return
-	--         end
-	--         local status = require("copilot.api").status.data
-	--         return colors[status.status] or colors[""]
-	--       end,
-	--     })
-	--   end,
-	-- },
+	{
+		"nvim-lualine/lualine.nvim",
+		optional = true,
+		event = "VeryLazy",
+		opts = function(_, opts)
+			local colors = {
+				[""] = fg("Special"),
+				["Normal"] = fg("Special"),
+				["Warning"] = fg("DiagnosticError"),
+				["InProgress"] = fg("DiagnosticWarn"),
+			}
+			table.insert(opts.sections.lualine_x, 2, {
+				function()
+					local icon = require("lazyvim.config").icons.kinds.Copilot
+					local status = require("copilot.api").status.data
+					return icon .. (status.message or "")
+				end,
+				cond = function()
+					local ok, clients = pcall(vim.lsp.get_active_clients, { name = "copilot", bufnr = 0 })
+					return ok and #clients > 0
+				end,
+				color = function()
+					if not package.loaded["copilot"] then
+						return
+					end
+					local status = require("copilot.api").status.data
+					return colors[status.status] or colors[""]
+				end,
+			})
+		end,
+	},
 }
