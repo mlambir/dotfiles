1acbdc9da58c61701cbaa6aed9cd71ccaf6c7c62
Mariano Lambir <mariano.lambir@shipveho.com>
2024-02-24 16:49:28 -0300

    switch the way cmp confirms

--- nvim/init.lua
+++ nvim/init.lua
@@ -652,6 +652,10 @@ cmp.setup {
     ['<C-b>'] = cmp.mapping.scroll_docs(-4),
     ['<C-f>'] = cmp.mapping.scroll_docs(4),
     ['<C-Space>'] = cmp.mapping.complete {},
+    ['<C-CR>'] = cmp.mapping.confirm {
+      behavior = cmp.ConfirmBehavior.Replace,
+      select = true,
+    },
     ['<CR>'] = cmp.mapping.confirm {
       behavior = cmp.ConfirmBehavior.Replace,
       select = false,
